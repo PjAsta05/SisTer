c8f9fd8f75eddbbd6c72423b99969ca6097f8f1b
PjAsta05 <114798198+PjAsta05@users.noreply.github.com>
2023-07-06 15:40:43 +0800

    Add files via upload

---
+++ LatWeb1/web/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="w-screen h-screen flex justify-center items-center">
+    <div class="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5">
+        <img class="w-32 h-32 rounded-full mx-auto object-cover" src="https://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/509541633-1687087927.png" alt="Profile picture">
+        <h2 class="text-center text-2xl font-semibold mt-3">Kadek Puja Astawa</h2>
+        <p class="text-center text-gray-600 mt-1">2115101005</p>
+        <div class="flex justify-center mt-5">
+            <a href="#" class="text-blue-500 hover:text-blue-700 mx-3">Twitter</a>
+            <a href="#" class="text-blue-500 hover:text-blue-700 mx-3">LinkedIn</a>
+            <a href="#" class="text-blue-500 hover:text-blue-700 mx-3">GitHub</a>
+        </div>
+        <div class="mt-5">
+        <h3 class="text-xl font-semibold">Bio</h3>
+        <p class="text-gray-600 mt-2">Puja is a software engineer with over 10 years of experience in developing web and mobile applications. He is skilled in JavaScript, React, and Node.js.</p>
+        </div>
+    </div>
+</body>
+</html>
